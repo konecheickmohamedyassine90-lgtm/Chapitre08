6fd4ff76fc8d4b0b4e779a746f9586d984bc9c3b
cheick <konecheickmohamedyassine90@gmail.com>
2026-05-28 11:34:35 +0000

    modification  de return

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -6,15 +6,17 @@ import java.util.Collections;
 import java.util.List;
 
 public class BookShelf {
+
     private final List<String> books = new ArrayList<>();
 
     public List<String> books() {
-        return books;
+        return Collections.unmodifiableList(books);
     }
 
     public void add(String bookToAdd) {
         books.add(bookToAdd);
     }
+
     public void add(String... booksToAdd) {
         books.addAll(Arrays.asList(booksToAdd));
     }
