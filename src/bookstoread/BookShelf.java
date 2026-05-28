38e82c2f8003a080f1197579c093f0f16bb00bcd
cheick <konecheickmohamedyassine90@gmail.com>
2026-05-28 11:04:16 +0000

    Notre methode a été amelioré

--- src/bookstoread/BookShelf.java
+++ src/bookstoread/BookShelf.java
@@ -1,6 +1,7 @@
 package bookstoread;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -13,5 +14,8 @@ public class BookShelf {
 
     public void add(String bookToAdd) {
         books.add(bookToAdd);
+    }
+    public void add(String... booksToAdd) {
+        books.addAll(Arrays.asList(booksToAdd));
     }
 }
